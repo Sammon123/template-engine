008e38039ff7133d798b1c4f8121eb24aedbd19c
Peter Sammon <petersammon2@gmail.com>
2020-06-10 16:11:08 -0400

    cli command prompt for inquirer

--- develop/app.js
+++ develop/app.js
@@ -1,18 +1,44 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+// const path = require("path");
+// const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+// const render = require("./lib/htmlRenderer");
 
 
 // Write code to use inquirer to gather information about the development team members,
+function runInquirer() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'what is your name',
+        },
+        {
+            type: 'input',
+            name: 'position',
+            message: 'what is your position',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'what is your id',
+        },
+        {
+            type: 'input',
+            name: 'role',
+            message: 'what is your role(school, Github, or office number)',
+        },
+    ])
+}
+runInquirer();
 // and to create objects for each team member (using the correct classes as blueprints!)
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
